b93ac8537f9abe7a5475680b3f57b413635317f1
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-05-20 23:26:18 +0700

    feat: pascal triangle

---
+++ 118. Pascal's Triangle/main.py
@@ -0,0 +1,29 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        def getNumber(nums: List[int]) -> List[int]:
+            """
+            Args:
+                nums List[int]: List of number that used to generated
+
+            Returns:
+                List[int]: New generated list
+            """
+            newList = []
+            if len(nums) == 0:
+                return [1]
+            if len(nums) == 1:
+                return [1, 1]
+            x = 0
+            y = 1
+            for i in range(1, len(nums)):
+                newList.append(nums[x]+nums[y])
+                x += 1
+                y += 1
+            return [1] + newList + [1]
+        
+        ans = [[1]]
+        for i in range(1, numRows):
+            newList = getNumber(ans[i-1])
+            ans.append(newList)
+        
+        return ans
