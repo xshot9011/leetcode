518593c6dff5606a1627f2595117a0c536ab9671
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-05-29 22:39:45 +0700

    feat: valid anagram

---
+++ 242. Valid Anagram/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        word = {}
+        for i in range(len(t)):
+            if t[i] in word:
+                word[t[i]] += 1
+            else:
+                word[t[i]] = 1
+        
+        for i in range(len(s)):
+            if s[i] in word:
+                word[s[i]] -= 1
+                if word[s[i]] == 0:
+                    del word[s[i]]
+            else:
+                return False
+
+        if word == {}:
+            return True
+        return False
