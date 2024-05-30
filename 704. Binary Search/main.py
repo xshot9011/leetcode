26391330b51b29221becb384e870d04d1f89cb9d
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-05-31 01:03:26 +0700

    feat: binary search

---
+++ 704. Binary Search/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binarySearch(left, right):
+            mid = left + (right-left)//2
+            if left > right:
+                return -1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                return binarySearch(left, mid-1)
+            elif nums[mid] < target:
+                return binarySearch(mid+1, right)
+        return binarySearch(0, len(nums)-1)
