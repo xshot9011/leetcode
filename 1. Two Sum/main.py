24b5786279aaea5e6492403c6985fb3925bfb39a
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-05-30 22:13:25 +0700

    feat: two sum

---
+++ 1. Two Sum/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = sorted(list(enumerate(nums)), key=lambda x: x[1])
+        array_size = len(nums)
+        first_pointer = 0
+        last_pointer = len(nums)-1
+        NUMBER_INDEX = 1
+        INDEX = 0
+        for i in range(0, array_size, 1):
+            summation = nums[first_pointer][NUMBER_INDEX] + nums[last_pointer][NUMBER_INDEX]
+            if summation == target:
+                return [nums[first_pointer][INDEX], nums[last_pointer][INDEX]]
+            elif summation > target:
+                last_pointer -= 1
+            else:
+                first_pointer += 1
