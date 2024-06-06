35311116a2789a5f6e884914fcb9b6c48d1e1dd5
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-06-07 01:00:50 +0700

    feat: climbing stairs

---
+++ climbing-stairs/main.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3: 
+            return n
+        
+        prev1 = 3
+        prev2 = 2
+        cur = 0
+
+        for _ in range(3, n):
+            cur = prev1 + prev2
+            prev2 = prev1
+            prev1 = cur
+        
+        return cur
