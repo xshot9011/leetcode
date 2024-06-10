142b516070841d827d6cf8a89bccd1ca5237dabc
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-06-10 07:36:45 +0700

    chore: optimize

--- excel-sheet-column-title/main.py
+++ excel-sheet-column-title/main.py
@@ -2,25 +2,16 @@
 
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        offset = 1
         base = 26
-        maxPossible = 0
-        cummulative = 0
         ans = ""
 
-        while maxPossible < columnNumber:
-            maxPossible += base ** offset
-            offset += 1 
-        
-        offset -= 1 # prevent issue from offset = 0
-        print(f'offset: {offset}')
-        print(f'maxPossible: {maxPossible}')
-        
-        for power in range(offset):
+        power = 0
+        while columnNumber > 0:
             factor = base**power
             char = int((columnNumber/(factor))%base)
             char = 26 if char == 0 else char
-            ans = 'Z' + ans if char == 26 else chr(ord('A')+char-1) + ans
+            ans = 'Z' + ans if char == 26 else chr(64+char) + ans
             columnNumber = columnNumber-(char*(factor))
+            power += 1
             
         return ans
