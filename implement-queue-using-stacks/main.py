434917c611f513c5e20bdbcbe4256f5db5b3cf74
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-06-12 05:08:32 +0700

    feat: optimize implement queue using stacks

--- implement-queue-using-stacks/main.py
+++ implement-queue-using-stacks/main.py
@@ -1,15 +1,20 @@
 # https://leetcode.com/problems/implement-queue-using-stacks/
 
 class MyQueue:
+    #       | Time   |
+    # push  |  O(1)  |
+    # pop   |  O(2n) |
+    # peek  |  O(2n) |
+    # empty |  O(1)  |
 
     def __init__(self):
         self.mainStack = []
         self.subStack = []
 
-    def push(self, x: int) -> None:
+    def push(self, x: int) -> None: # O(1)
         self.mainStack.append(x)
 
-    def pop(self) -> int:
+    def pop(self) -> int: # O(2n)
         for i in range(len(self.mainStack)):
             self.subStack.append(self.mainStack.pop())
         lastElement = self.subStack.pop()
@@ -30,6 +35,47 @@ class MyQueue:
 
     def empty(self) -> bool:
         return True if len(self.mainStack) == 0 else False
+
+class MyQueue:
+    #       | Time   |
+    # push  |  O(1)  |
+    # pop   |  O(2n) |
+    # peek  |  O(2n) |
+    # empty |  O(1)  |
+
+    def __init__(self):
+        self.mainStack = []
+        self.subStack = []
+        self.isCurrentMainStack = True
+
+    def push(self, x: int) -> None:
+        if not self.isCurrentMainStack:
+            for i in range(len(self.subStack)):
+                self.mainStack.append(self.subStack.pop())
+        self.mainStack.append(x)
+        self.isCurrentMainStack = True
+
+    def pop(self) -> int:
+        if self.isCurrentMainStack:
+            for i in range(len(self.mainStack)):
+                self.subStack.append(self.mainStack.pop())
+            self.isCurrentMainStack = False
+        return self.subStack.pop()
+
+    def peek(self) -> int:
+        if self.isCurrentMainStack:
+            for i in range(len(self.mainStack)):
+                self.subStack.append(self.mainStack.pop())
+            self.isCurrentMainStack = False
+        return self.subStack[-1]
+
+    def empty(self) -> bool:
+        pointer = None
+        if self.isCurrentMainStack:
+            pointer = self.mainStack
+        else:
+            pointer = self.subStack 
+        return True if len(pointer) == 0 else False
         
 
 
