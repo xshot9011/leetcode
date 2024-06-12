24e16d69c7da32953cfe6f37b9117e7709ed9124
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-06-12 23:27:27 +0700

    feat: add digits

---
+++ add-digits/main.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/add-digits/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        ans = 0
+        while True:
+            while num > 0:
+                ans += num%10
+                num //= 10
+            if ans >= 10:
+                num = ans
+                ans = 0
+            else:
+                return ans
+        return ans
