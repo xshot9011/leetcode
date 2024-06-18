83cb20f097d68b848cc65f5ac3261e3a8268d5f4
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2024-06-18 09:09:14 +0700

    feat: is subsequence

---
+++ is-subsequence/main.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sIndex = 0
+        tIndex = 0
+        sLen = len(s)
+        tLen = len(t)
+
+        while tIndex < tLen and sIndex < sLen:
+            if s[sIndex] == t[tIndex]:
+                sIndex += 1
+
+            tIndex += 1
+
+        return sIndex == len(s) # Last match get +1 == len(s)
